82a787e5ba1aea3c9138fd376d432e626f0917dd
rugemini1214 <ru1214op@gmail.com>
2025-12-05 23:48:01 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'buy-or-not-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+  'https://unpkg.com/@phosphor-icons/web',
+  'https://cdn-icons-png.flaticon.com/512/3081/3081559.png'
+];
+
+// 安裝時：快取所有靜態資源
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+// 連線時：優先使用快取，失敗則連網 (Cache First Strategy)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // 如果快取有，直接回傳
+        if (response) {
+          return response;
+        }
+        // 如果快取沒有，去網路抓
+        return fetch(event.request);
+      })
+  );
+});
+
+// 更新時：清除舊快取
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
